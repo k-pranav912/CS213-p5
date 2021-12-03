ce06c04234a5273638b94483dc2efdfcebbca90a
pranavkalapala <saipranav1289@gmail.com>
2021-12-03 13:13:11 -0500

    Initial commit

---
+++ app/src/main/java/com/pizzastore/MainActivity.java
@@ -0,0 +1,13 @@
+package com.pizzastore;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.current_order);
+    }
+}
